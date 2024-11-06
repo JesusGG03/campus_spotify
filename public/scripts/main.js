409f8362a3cb1fda0b388f4a5eca0fa4ce32830e
JesusGG03 <jesusgg2704@gmail.com>
2024-11-06 17:48:29 -0500

    Fix: Attempted refactor for genres

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -397,6 +397,7 @@ function displayUniGenreChart(genreData) {
                 label: 'Genre Distribution',
                 data: data,
                 backgroundColor: [
+                    '#FF6384', '#36A2EB', '#FFCE56', '#4BC0C0', '#9966FF', '#FF9F40',
                     '#FF6384', '#36A2EB', '#FFCE56', '#4BC0C0', '#9966FF', '#FF9F40'
                 ]
             }]
